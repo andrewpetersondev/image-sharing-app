b04cb2e595e0a0a9d40e89dd76b4756a51a192eb
Andrew Peterson <andrewpetersondev@gmail.com>
2023-12-20 17:58:42 -0600

    more client starter files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { loginSuccess } from "./pages/userSlice"
 
-function App() {
-  const [count, setCount] = useState(0)
+// Pages
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Profile from "./pages/profile/Profile"
+
+const App = () => {
+  const { user } = useSelector((state) => state.user)
+
+  const dispatch = useDispatch()
+
+  // Check if the user is logged in or not
+  useEffect(() => {
+    // Get the user from local storage
+    const loggedInUser = localStorage.getItem("user")
+    // If the user is logged in then dispatch the loginSuccess action
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser)
+      dispatch(loginSuccess(foundUser))
+    }
+  }, [dispatch])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        {/* If a user is logged in then go to the homepage otherwise the register page */}
+        <Route
+          exact
+          path='/'
+          element={user ? <Home /> : <Register />}
+        />
+        {/* If the user is on the login page and user is already logged in then go to the homepage otherwise the login page */}
+        <Route
+          path='/login'
+          element={
+            user ? (
+              <Navigate
+                replace
+                to={"/"}
+              />
+            ) : (
+              <Login />
+            )
+          }
+        />
+        {/* If the user is on the register page and user is already logged in then go to the homepage otherwise the register page */}
+        <Route
+          path='/register'
+          element={
+            user ? (
+              <Navigate
+                replace
+                to={"/"}
+              />
+            ) : (
+              <Register />
+            )
+          }
+        />
+        <Route
+          exact
+          path='/profile/:username'
+          element={<Profile />}
+        />
+      </Routes>
+    </Router>
   )
 }
-
 export default App
